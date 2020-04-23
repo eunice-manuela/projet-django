368d72c8c4f961018ce6832fe354b751bcff2c51
dals64 <lemotieuarold@gmail.com>
2020-04-23 12:50:05 +0200

    asasa

--- projet-back1/Terminaux_perdus/models.py
+++ projet-back1/Terminaux_perdus/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class Terminaux_perdus(models.Model):
+    client = models.CharField(max_length=36)
+    identifiant_terminal = models.CharField(max_length=50)
+    date_création = models.DateTimeField(auto_now_add=True)
